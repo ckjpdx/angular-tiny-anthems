a380bb7c6deacbd0de53e1c11c05f9d5dd285ed5
Chris Knight Johnson <cknightjohnson@gmail.com>
2018-01-25 10:30:38 -0800

    add landing component and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
+import { routing } from './app.routing';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
